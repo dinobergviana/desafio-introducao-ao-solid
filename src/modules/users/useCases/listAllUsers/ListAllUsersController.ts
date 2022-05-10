f52d26bf4fcdb4537aa50f46797d776f2cff3779
dinobergviana <dinobergueviana@gmail.com>
2022-05-10 10:09:04 -0300

    ajuste final

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,12 +6,14 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { user_id } = request.params;
+    const { user_id } = request.headers;
     try {
-      const users = this.listAllUsersUseCase.execute({ user_id });
+      const users = this.listAllUsersUseCase.execute({
+        user_id: String(user_id),
+      });
       return response.json(users);
     } catch (err) {
-      return response.status(404).json({ error: err.message });
+      return response.status(400).json({ error: err.message });
     }
   }
 }
